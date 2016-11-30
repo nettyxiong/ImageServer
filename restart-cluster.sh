83a6f45833039f0743d77afbe5cf0439edcb9644
sxiong <sxiong@hust.edu.cn>
2016-11-30 08:53:03 +0800

    add restart-cluster.sh

---
+++ restart-cluster.sh
@@ -0,0 +1,15 @@
+
+sudo docker stop some-redis
+sudo docker stop hadoop-master
+sudo docker stop hadoop-slave1
+sudo docker stop hadoop-slave2
+sudo docker stop hadoop-slave3
+
+sudo docker start hadoop-slave1
+sudo docker start hadoop-slave2
+sudo docker start hadoop-slave3
+sudo docker start hadoop-master
+sudo docker start some-redis
+
+sudo docker exec -it hadoop-master bash
+
